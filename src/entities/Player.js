b5e72162f29fb5769d49f477d5ca191b9a0a1cd3
jvbc <joao@example.com>
2024-12-14 21:25:09 -0400

    Adicionada o esboço do array de perguntas com as posições em azul

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -18,20 +18,99 @@ class Player {
 
         // Questões e alternativas mapeadas
         this.questoes = [
-            new Question(1, "Qual é a capital do Brasil?", [1, 2, 3, 4]),
-            new Question(2, "Qual é a maior montanha do mundo?", [1, 2, 3, 4])
+            new Question(1, "enunciado"),//azul
+            new Question(2, "enunciado"),
+            new Question(3, "enunciado"),//azul
+            new Question(4, "enunciado"),
+            new Question(5, "enunciado"),//azul
+            new Question(6, "enunciado"),
+            new Question(7, "enunciado"),
+            new Question(8, "enunciado"),//azul
+            new Question(9, "enunciado"),
+            new Question(10, "enunciado"),//azul
+            new Question(11, "enunciado"),//azul
+            new Question(12, "enunciado"),
+            new Question(13, "enunciado"),//azul
+            new Question(14, "enunciado"),
+            new Question(15, "enunciado"),//azul
+            new Question(16, "enunciado"),//azul
+            new Question(17, "enunciado"),//azul
+            new Question(18, "enunciado"),
+            new Question(19, "enunciado"),
+            new Question(20, "enunciado"),
+            new Question(21, "enunciado"),//azul
+            new Question(22, "enunciado"),//azul
+            new Question(23, "enunciado"),
+            new Question(24, "enunciado"),//azul
+            new Question(25, "enunciado"),//azul
+            new Question(26, "enunciado"),
+            new Question(27, "enunciado"),//azul
+            new Question(28, "enunciado"),
+            new Question(29, "enunciado"),
+            new Question(30, "enunciado"),//azul
+            new Question(31, "enunciado"),
+            new Question(32, "enunciado"),//azul
+            new Question(33, "enunciado"),
+            new Question(34, "enunciado"),//azul
+            new Question(35, "enunciado"),
+            new Question(36, "enunciado"),//azul
+            new Question(37, "enunciado"),
+            new Question(38, "enunciado"),//azul
+            
+            // new Question(num, "enunciado")
+
         ];
 
         this.alternativas = [
             new Alternative(1, 1, ["Belo Horizonte", "São Paulo", "Brasília", "Cuiabá"], "Brasília"),
-            new Alternative(2, 2, ["Everest", "Kilimanjaro", "Aconcágua", "Mont Blanc"], "Everest")
+            new Alternative(2, 2, ["Everest", "Kilimanjaro", "Aconcágua", "Mont Blanc"], "Everest"),
+            new Alternative(3, 3, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(4, 4, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(5, 5, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(6, 6, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(7, 7, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(8, 8, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(9, 9, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(10, 10, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(11, 11, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(12, 12, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(13, 13, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(14, 14, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(15, 15, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(16, 16, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(17, 17, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(18, 18, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(19, 19, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(20, 20, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(21, 21, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(22, 22, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(23, 23, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(24, 24, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(25, 25, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(26, 26, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(27, 27, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(28, 28, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(29, 29, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(30, 30, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(31, 31, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(32, 32, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(33, 33, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(34, 34, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(35, 35, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(36, 36, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(37, 37, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+            new Alternative(38, 38, ["A_1", "A_2", "A_3", "A_4"], "RESP"),
+
+
+            //new Alternative(id_alt, id_perg, ["A_1", "A_2", "A_3", "A_4"], "RESP")
         ];
         
 
         // Mapeamento de questões por casa
         this.questoesPorCasa = [
-            { casa: 1, pergunta: this.questoes[0], alternativas: this.alternativas.slice(0, 1) },  // Alternativa 1
-            { casa: 2, pergunta: this.questoes[1], alternativas: this.alternativas.slice(1, 2) }   // Alternativa 2
+            { casa: 1, pergunta: this.questoes[1], alternativas: this.alternativas.slice(1, 2) },  // Alternativa 1
+            { casa: 2, pergunta: this.questoes[2], alternativas: this.alternativas.slice(2, 3) }   // Alternativa 2
+            //{ casa: 'posicao', pergunta: this.questoes[posicao], alternativas: this.alternativas.slice(prim_valor, seg_valor) }
         ];
         
     }
